be80e8d822eedd3423c31b863b06ddfa357a5a6f
kamathamsaimanikanta <kamathamsaimanikanta@gmail.com>
2025-07-16 15:53:41 +0530

    Update Flipkart9.java

--- Flipkart9.java
+++ Flipkart9.java
@@ -2,5 +2,7 @@ public class Flipkart{
   Public static void main(String[] args)
   {
       System.out.println("Flipkart9");
+    System.out.println("Flipkart9");
+    System.out.println("Flipkart9");
   }
 }
